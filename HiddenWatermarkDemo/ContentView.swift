0033b9dbb83749effef3846cfbc883fc56fd00e0
LiYanan2004 <1474700628@qq.com>
2023-08-02 15:00:25 +0800

    Initial Commit

---
+++ HiddenWatermarkDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HiddenWatermarkDemo
+//
+//  Created by LiYanan2004 on 2023/8/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HiddenWatermarkDemo/HiddenWatermarkDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiddenWatermarkDemoApp.swift
+//  HiddenWatermarkDemo
+//
+//  Created by LiYanan2004 on 2023/8/2.
+//
+
+import SwiftUI
+
+@main
+struct HiddenWatermarkDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
